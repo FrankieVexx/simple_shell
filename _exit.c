1b6df4adfc248d19d46c826287498df93813773c
Francis Onyach <franblog7@gmail.com>
2022-11-16 04:49:09 -0800

    edit exit file

--- _exit.c
+++ _exit.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * *_strncpy - copies a string
- * @dest: the destination tring to be copied to
- * @src: the source string
- * @n: the amount of characters to ne copied
- * Return: the concatenated string
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	char *s = dest;
 
 	i = 0;
-	while (src[1] != '\0' && i < n - 1)
+	while (src[i] != '\0' && i < n - 1)
 	{
 		dest[i] = src[i];
 		i++;
@@ -31,13 +31,13 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- * *strncat - concatenates two strings
- * @dest: the first string
- * @src: the second string
- * @n: the amount of bytesto be maximally  used
- * Return: the conacatenated string
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
-char *strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
@@ -58,10 +58,10 @@ char *strncat(char *dest, char *src, int n)
 }
 
 /**
- * *_strchr - locates a character string
- * @s: the string to be passed
- * @c: the charcter to look for
- * Return: (s) a pointer to the memory area s
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
@@ -69,5 +69,6 @@ char *_strchr(char *s, char c)
 		if (*s == c)
 			return (s);
 	} while (*s++ != '\0');
+
 	return (NULL);
 }
